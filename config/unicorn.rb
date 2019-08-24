86373df7b4bb0dc90fb1a5fc494833f7113afde1
加藤拓磨 <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-08-24 22:37:33 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,6 +10,7 @@ working_directory "#{app_path}/current"
 #Unicornの起動に必要なファイルの設置場所を指定
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 
+#ポート番号を指定
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 
 #エラーログを記録するファイルを指定
@@ -17,9 +18,6 @@ stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 
 #通常のログを記録するファイルを指定
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
-
-#ポート番号を指定
-listen 3000
 
 #Railsアプリケーションの応答をもつ上限時間を設定
 timeout 60
