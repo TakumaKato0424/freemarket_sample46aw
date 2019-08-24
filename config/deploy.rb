f1e7487c447c73f67069b3a85ce6aea4a61eea9f
加藤拓磨 <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-08-25 01:15:11 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,36 +1,29 @@
-# config valid only for current version of Capistrano
-# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock '3.11.0'
+lock "~> 3.11.0"
 
-# Capistranoのログの表示に利用する
 set :application, 'freemarket_sample46aw'
-
-# どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:TakumaKato0424/freemarket_sample46aw.git'
 
-# バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+set :rbenv_ruby, '2.5.1'
 
-# どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/key_arutonicoX.pem']
+                  keys: ['~/.arutonicoX.pem']
 
-# プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
-
-# Unicornの設定ファイルの場所
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
-# デプロイ処理が終わった後、Unicornを再起動するための記述
+set :linked_files, %w{ config/master.key }
+
+
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+
   desc 'upload master.key'
   task :upload do
     on roles(:app) do |host|
@@ -43,3 +36,10 @@ namespace :deploy do
   before :starting, 'deploy:upload'
   after :finishing, 'deploy:cleanup'
 end
+
+set :default_env, {
+  rbenv_root: "/usr/local/rbenv",
+  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+  BASIC_AUTH_USER: ENV["BASIC_AUTH_USER"],
+  BASIC_AUTH_PASSWORD: ENV["BASIC_AUTH_PASSWORD"]
+}
